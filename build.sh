1428de5e3b59312474197e7294fd2f0419a8fea6
Lachlan Gleeson <lgleeson@au1.ibm.com>
2019-07-03 10:52:23 +1000

    use setup.py not twist

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ export HOME=$( realpath . )
 
 pip install twine wheel
 
-python setup.py sdist bdist_wheel
+python setup.py sdist bdist_wheel upload -r artifactory
 
-twine upload --verbose --repository-url https://eu.artifactory.swg-devops.com --repository /artifactory/sec-iam-components-pypi-local/legacy/ -u lgleeson@au1.ibm.com -p $ART_API_KEY dist/*
+#twine upload --verbose --repository-url https://eu.artifactory.swg-devops.com --repository /artifactory/sec-iam-components-pypi-virtual -u lgleeson@au1.ibm.com -p $ART_API_KEY dist/*
