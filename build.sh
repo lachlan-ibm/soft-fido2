117383f10f195b1e3dc9cf79845d3ed956b8ff1a
Lachlan Gleeson <lgleeson@au1.ibm.com>
2019-07-03 10:45:24 +1000

    use twine to publish

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ pip install twine wheel
 
 python setup.py sdist bdist_wheel
 
-twine upload --repository-url https://eu.artifactory.swg-devops.com/artifactory/sec-iam-components-pypi-local/legacy/ -u lgleeson@au1.ibm.com -p $ART_API_KEY dist/*
+twine upload --verbose --repository-url https://eu.artifactory.swg-devops.com --repository /artifactory/sec-iam-components-pypi-local/legacy/ -u lgleeson@au1.ibm.com -p $ART_API_KEY dist/*
