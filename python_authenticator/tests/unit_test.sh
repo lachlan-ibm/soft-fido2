970e96d7f739800be1b49b3236db17a68c8a7064
lgleeson <lgleeson@au1.ibm.com>
2020-04-14 06:52:54 +1000

    fix unit tests

--- python_authenticator/tests/unit_test.sh
+++ python_authenticator/tests/unit_test.sh
@@ -3,11 +3,10 @@
 set -e
 
 #compile
+pip install -r requirements.txt
 python setup.py sdist bdist_wheel
-
 #run tests
 export PYTHONPATH="$PYTHONPATH:$(pwd)/build/lib"
-ls $(pwd)/build/lib
 cd python_authenticator/tests
 python3 attestation_unit_test.py 
 python3 assertion_unit_test.py
