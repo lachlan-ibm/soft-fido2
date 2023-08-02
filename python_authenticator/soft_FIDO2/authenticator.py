8676dee25bd29a4aff8cae0e95b63b874df9f97e
Lachlan Gleeson <lgleeson@au1.ibm.com>
2023-08-03 08:46:57 +1000

    re-enable uv

--- python_authenticator/soft_FIDO2/authenticator.py
+++ python_authenticator/soft_FIDO2/authenticator.py
@@ -422,8 +422,8 @@ class Fido2Authenticator(object):
             flags |= 0x01  # UP
         if not assertion:
             flags |= 0x40  # AT
-        #if attStmtFmt != 'fido-u2f' and uv:
-        #    flags |= 0x04  # UV
+        if attStmtFmt != 'fido-u2f' and uv:
+            flags |= 0x04  # UV
         authDataBytes += struct.pack("c", chr(flags).encode('utf-8'))
         #Add counter and increment
         authDataBytes += struct.pack("I", self.counter)
