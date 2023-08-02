03aa7610d4c11fc85e6def54ccbfc7afc644c9ae
Lachlan Gleeson <lgleeson@au1.ibm.com>
2023-08-03 08:02:16 +1000

    version bump for reset with v3 travis

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name='soft_FIDO2',
-    version='0.1.%s' % os.environ.get('TRAVIS_BUILD_NUMBER', 0),
+    version='0.2.%s' % os.environ.get('TRAVIS_BUILD_NUMBER', 0),
     description='Software based FIDO2 Authenticator',
     author='Lachlan Gleeson',
     author_email='lgleeson@au1.ibm.com',
