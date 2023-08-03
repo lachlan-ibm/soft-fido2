399a0ce2174b9e83207ed403196e26ef7d1bba6b
Lachlan Gleeson <lgleeson@au1.ibm.com>
2023-08-04 08:34:23 +1000

    Fix error in running tests

--- python_authenticator/tests/conftest.py
+++ python_authenticator/tests/conftest.py
@@ -18,7 +18,7 @@ def fido2_server():
     return Fido2Server(rp)
 
 @pytest.fixture(scope='module')
-def fido2_autenticator(fido2_server, fido2_user)
+def fido2_autenticator(fido2_server, fido2_user):
     from soft_FIDO2 import Fido2Authenticator
     attestation_options = fido2_server().register_begin(fido2_user)
     authenticator = Fido2Authenticator()
