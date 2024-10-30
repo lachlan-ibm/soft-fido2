c4f5423d6612f487570752fdf2be6ec514cabc3e
Lachlan Gleeson <lgleeson@au1.ibm.com>
2024-10-31 08:32:01 +1000

    Add in support for compound attestation

--- python_authenticator/tests/attestation_unit_test.py
+++ python_authenticator/tests/attestation_unit_test.py
@@ -11,6 +11,7 @@ from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import ec
 
 
 
