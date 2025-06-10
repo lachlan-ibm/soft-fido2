174b98b77b4aef544e774e03d457d3348309eac1
Lachlan Gleeson <lgleeson@au1.ibm.com>
2025-06-11 05:43:06 +1000

    dix defects in uhid_device.py

--- python_authenticator/soft_fido2/uhid_device.py
+++ python_authenticator/soft_fido2/uhid_device.py
@@ -3,14 +3,61 @@
 # Assisted by watsonx Code Assistant
 #Copyright IBM Corp. 2025
 
-import os, struct, fcntl, errno, time, queue, threading
+import os, struct, fcntl, errno, time, queue, threading, logging, re, signal
 
 from enum import Enum
 
-from .hid_device import CTAP2HIDevice
+# Assisted by watsonx Code Assistant 
+logging.basicConfig(filename='passkey.log', filemode='a', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+# Hey StackOverflow !
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    OKPINK = '\033[95m'
+    OKYELLOW = '\033[93m'
+    OKPURPLE = '\033[35m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def colour_print(colour=bcolors.OKBLUE, component='USB/IP', msg=''):
+    logging.debug('[' + colour + component + bcolors.ENDC + '] ' + msg)
+
+
+def print_bytes(*args):
+    result = ""
+    count = 0
+    for ba in args:
+        for x in ba:
+            result += "%02X " % x
+            count += 1
+            if count == 8 :
+                result += " "
+            elif count == 16:
+                logging.debug("\t" + result)
+                result = ""
+                count = 0
+    logging.debug('\t' + result + '\n')
+
+def dump_bytes(*args, colour=bcolors.OKPURPLE, component='UHID Device', msg=''):
+    #Print bytes in nice format
+    c = colour if colour != None else bcolors.OKPURPLE
+    colour_print(colour=colour, component=component, msg=msg)
+    print_bytes(*args)
+
 
 UHID_EVENT_TYPE_SIZE = 4
+'''
+uint_32_t type;
+'''
 
+EV_MAX_SIZE = 4380
 '''
 /**
  * Fedora 40 test.
@@ -32,7 +79,6 @@ int main(int argc, char **argv)
     return 0;
 }
 '''
-EV_MAX_SIZE = 4380
 
 
 class UHIDEventType(Enum):
@@ -70,7 +116,7 @@ class UHIDReportType(Enum):
     OUTPUT_REPORT = 0x01
     INPUT_REPORT = 0x02
 
-class BaseEvent(object):
+class BaseStructure(object):
     _fields = []
 
     def __init__(self, **kwargs):
@@ -88,9 +134,9 @@ class BaseEvent(object):
         return struct.calcsize(self.format())
 
     def format(self):
-        pack_format = '>'
+        pack_format = '<'
         for field in self._fields_:
-            if isinstance(field[1], BaseEvent):
+            if isinstance(field[1], BaseStructure):
                 pack_format += str(field[1].size()) + 's'
             elif 'si' == field[1]:
                 pack_format += 'c'
@@ -98,14 +144,14 @@ class BaseEvent(object):
                 pack_format += field[1][1:]
             else:
                 pack_format += field[1]
-        #print(pack_format)
+        #logging.debug(pack_format)
         return pack_format.encode('utf-8')
 
     def pack(self):
         values = []
         for field in self._fields_:
-            #print("Field: {}".format(field))
-            if isinstance(field[1], BaseEvent):
+            #logging.debug("Field: {}".format(field))
+            if isinstance(field[1], BaseStructure):
                 values.append(getattr(self, field[0], 0).pack())
             elif re.match(r'\d*x', field[1]):
                 #Skip padding
@@ -117,9 +163,9 @@ class BaseEvent(object):
                     values.append(getattr(self, field[0], 0))
         #Python 2 -> 3, str != bytestring so conditionally remap any strings we find.
         values = [bytes(v, 'utf-8') if isinstance(v, str) else v for v in values]
-        #print(values)
+        #logging.debug(values)
         packed = struct.pack(self.format(), *values)
-        #print("packed [{}]".format(packed))
+        #logging.debug("packed [{}]".format(packed))
         return packed
 
     def unpack(self, buf):
@@ -131,7 +177,7 @@ class BaseEvent(object):
                 val = struct.unpack('<' +self._fields_[i][1][1], struct.pack('>' + self._fields_[i][1][1], val))[0]
             keys_vals[self._fields_[i][0]]=val
             i+=1
-        #print(keys_vals)
+        #logging.debug(keys_vals)
         self.init_from_dict(**keys_vals)
 
 REPORT_DESCRIPTOR = bytes([
@@ -139,21 +185,20 @@ REPORT_DESCRIPTOR = bytes([
     0x09, 0x01, # Usage (U2F HID Authenticator)
     0xA1, 0x01, # Collection (Application)
 
-    0x85, 0x01, # Report ID (1)
     0x09, 0x20, # Usage (Input Report Data)
     0x15, 0x00, # Logical Minimum (0)
     0x26, 0xFF, 0x00, # Logical Maximum (255)
-    0x75, 0x08, # Report Size (8)
-    0x95, 0x40, # Report Count (64)
+    0x75, 0x08,             # REPORT_SIZE (8)
+    0x95, 0x40,             # REPORT_COUNT (64)
     0x81, 0x02, # Input (Data,Var,Abs)
 
-    0x85, 0x01, # Report ID (1)
     0x09, 0x21, # Usage (Output Report Data)
     0x15, 0x00, # Logical Minimum (0)
     0x26, 0xFF, 0x00, # Logical Maximum (255)
-    0x75, 0x08, # Report Size (8)
-    0x95, 0x40, # Report Count (64)
+    0x75, 0x08,             # REPORT_SIZE (8)
+    0x95, 0x40,             # REPORT_COUNT (64)
     0x91, 0x02, # Output (Data,Var,Abs)
+
     0xC0 # End Collection
 ])
 
@@ -161,44 +206,60 @@ DEVICE_NAME = b"EyeBeeKey"
 PHYSICAL_ADDRESS = b"ibm-0101:01:01.0-1"
 UNIQUE_ADDRESS = b"virtual-fido-uhid-01"
 
-class UHIDCreate2Event(BaseEvent):
+class UHIDCreate2Event(BaseStructure):
     _fields_ = [
             ('event', 'I', UHIDEventType.CREATE2.value),
             ('name', '128s', DEVICE_NAME.ljust(128, b'\x00')),
             ('phys', '64s', PHYSICAL_ADDRESS.ljust(64, b'\x00')),
             ('uniq', '64s', UNIQUE_ADDRESS.ljust(64, b'\x00')),
             ('rd_size', 'H', len(REPORT_DESCRIPTOR)),
-            ('bus', 'H', 0x03), #BUS_USB
-            ('vendor', 'I', 0x3713),
-            ('product', 'I', 0x3713),
+            ('bus', 'H', 0x03), # USB HID
+            ('vendor', 'I', 0x1337),
+            ('product', 'I', 0x1337),
             ('version', 'I', 0x0100), # Version 1.00
             ('country', 'I', 0), # Not localized
             ('rd_data', '4096s', REPORT_DESCRIPTOR.ljust(4096, b'\x00'))
         ]
 
-class UHIDStartEvent(BaseEvent):
+class UHIDStartEvent(BaseStructure):
     _fields_ = [
             ('event', 'I', UHIDEventType.START.value),
             ('dev_flags', 'Q')
         ]
 
-class UHIDInput2Event(BaseEvent):
+class UHIDStopEvent(BaseStructure):
+    _fields_ = [
+            ('event', 'I', UHIDEventType.STOP.value)
+        ]
+
+class UHIDInput2Event(BaseStructure):
 
     _fields_ = [
             ('event', 'I', UHIDEventType.INPUT2.value),
-            ('size', 'H'),
-            ('data', '4096s')
+            ('ev_len', 'H', 0),
+            ('data', '4096s', b'\x00' * 4096 )
         ]
 
-class UHIDOutputEvent(BaseEvent):
+class UHIDOutputEvent(BaseStructure):
      _fields_ = [
             ('event', 'I', UHIDEventType.OUTPUT.value),
-            ('data', '4096s'),
-            ('size', 'H'),
-            ('type', 'I')
+            ('data', '4096s', b'\x00' * 4096),
+            ('ev_len', 'H', 0),
+            ('type', 'B', UHIDReportType.OUTPUT_REPORT.value)
         ]
 
-class UHIDGetReportReq(BaseEvent):
+
+class UHIDOpenEvent(BaseStructure):
+    _fields_ = [
+            ('event', 'I', UHIDEventType.OPEN.value)
+        ]
+
+class UHIDCloseEvent(BaseStructure):
+    _fields_ = [
+            ('event', 'I', UHIDEventType.CLOSE.value)
+        ]
+
+class UHIDGetReport(BaseStructure):
     _fields_ = [
             ('event', 'I', UHIDEventType.GET_REPORT.value),
             ('id', 'I'),
@@ -206,26 +267,26 @@ class UHIDGetReportReq(BaseEvent):
             ('report_type', 'B')
         ]
 
-class UHIDGetReportReply(BaseEvent):
+class UHIDGetReportReply(BaseStructure):
     _fields_ = [
             ('event', 'I', UHIDEventType.GET_REPORT_REPLY.value),
             ('id', 'I'),
             ('err', 'H'),
-            ('size', 'H'),
+            ('ev_len', 'H'),
             ('data', '4096s')
         ]
 
-class UHIDSetReportReq(BaseEvent):
+class UHIDSetReport(BaseStructure):
     _fields_ = [
             ('event', 'I', UHIDEventType.SET_REPORT.value),
             ('id', 'I'),
             ('report_number', 'B'),
             ('report_type', 'B'),
-            ('size', 'H'),
+            ('ev_len', 'H'),
             ('data', '4096s')
         ]
 
-class UHIDSetReportReply(BaseEvent):
+class UHIDSetReportReply(BaseStructure):
     _fields_ = [
             ('event', 'I', UHIDEventType.SET_REPORT_REPLY.value),
             ('id', 'I'),
@@ -234,104 +295,169 @@ class UHIDSetReportReply(BaseEvent):
 
 class UserDevice(threading.Thread):
 
-    def __init__(self, device_path="/dev/uhid"):
-        self.devPath = device_path
-        self.inQ = queue.LifoQueue()
-        self.ctapDev= CTAP2HIDevice()
+    #Pending input reports
+    pending = queue.Queue(maxsize=10)
+    #Get reports awaiting a reply
+    inQ = queue.Queue(maxsize=10)
 
-    def start_ev(self, ev_type, ev):
-        print("Start event received!")
+    def __init__(self, devPath="/dev/uhid"):
+        super().__init__()
+        self.device_path = devPath
+        self.interrupt = False
+        signal.signal(signal.SIGINT, self.interrupt)
+        signal.signal(signal.SIGTERM, self.interrupt)
+
+    def interrupt():
+        self.interrupt = True
+
+    # Assisted by watsonx Code Assistant 
+    def format_bytes(self, byte_array):
+        return ''.join(f'{byte:02x}' for byte in byte_array)
+
+    # Assisted by watsonx Code Assistant 
+    def log_received_bytes(self, byte_array):
+        logging.debug("event byte count: {}".format( 
+                            0 if byte_array is None else len(byte_array)))
+        formatted_bytes = self.format_bytes(byte_array)
+        lines = [formatted_bytes[i:i+64] for i in range(0, len(formatted_bytes), 64)]
+        for line in lines:
+            logging.debug(line)
+
+    def start_ev(self, ev_type, ev_bytes):
+        logging.debug("Start event received!")
+        ev = UHIDStartEvent()
+        ev.unpack(ev_bytes[:len(ev.pack())])
+        logging.debug(f"ev: {ev}")
         return
 
-    def stop_ev(self, ev_type, ev):
-        print("Stop event received")
+    def stop_ev(self, ev_type, ev_bytes):
+        logging.debug("Stop event received")
+        ev = UHIDStopEvent()
+        ev.unpack(ev_bytes[:len(ev.pack())])
+        logging.debug(f"ev: {ev}")
         return
 
-    def open_ev(self, ev_type, ev):
-        print("Open event received!")
+    def open_ev(self, ev_type, ev_bytes):
+        logging.debug("Open event received!")
+        ev = UHIDOpenEvent()
+        ev.unpack(ev_bytes[:len(ev.pack())])
+        logging.debug(f"ev: {ev}")
         return
 
-    def close_ev(self, ev_type, ev):
-        print("Close event received!")
+    def close_ev(self, ev_type, ev_bytes):
+        logging.debug("Close event received!")
+        ev = UHIDCloseEvent()
+        ev.unpack(ev_bytes[:len(ev.pack())])
+        logging.debug(f"ev: {ev}")
         return
 
-    def output_ev(self, ev_type, ev):
-        return ctapDev.handle_data(ev)
+    def process_output(self, event):
+        raise NotImplemented("override me")
 
-    def report_ev(self, ev_type, ev):
-        print("Get report event received!")
-        inQ.put(UHIDGetReportReply(
-            id=req.id,
-            err=0,
-            size=len(report_data),
-            data=report_data.ljust(4096, b'\x00')
-        ).pack())
+    def output_ev(self, ev_type, ev_bytes):
+        logging.debug("Output event received!")
+        ev = UHIDOutputEvent()
+        ev.unpack(ev_bytes[:len(ev.pack())])
+        logging.debug(f"event : {ev.ev_len} {ev.type} {ev.data[:ev.ev_len]}")
+        self.process_output(ev)
+
+    def handle_unknown_control(self, ev_bytes):
+        raise NotImplemented("override me")
+
+    def get_report_ev(self, ev_type, ev_bytes):
+        logging.debug("Get report event received!")
+        ev = UHIDGetReport()
+        ev.unpack(ev_bytes[:len(ev.pack())])
+        logging.debug(f"ev: {ev}")
+        if ev.report_type == UHIDReportType.INPUT_REPORT:
+            self.inQ.put(ev)
+        elif getReport.report_type == UHIDReportType.FEATURE_REPORT:
+            self.handle_unknown_control(ev_bytes)
+        else:
+            raise NotImplementedError("unexpected request")
+
+    def set_report_ev(self, ev_type, ev_bytes):
+        logging.debug("Set report event received!")
+        ev = UHIDSetReport()
+        ev.unpack(ev_bytes[:len(ev.pack())])
+        logging.debug(f"ev: {ev}")
+        if ev.report_type == UHIDReportType.FEATURE_REPORT:
+            raise NotImplemented("TODO")
+        else:
+            raise NotImplementedError("unexpected request")
+
+    def error_ev(self, ev_type, ev_bytes):
+        logging.debug("Unknown UHID event received")
+        logging.debug(f"ev_bytes: {ev_bytes}")
         return
 
-    def error_ev(self, ev_type, ev):
-        print("Unknown UHID event received")
-        return
-
-    def process_event(self, ev_type, ev):
+    def process_event(self, ev_type, ev_bytes):
         return {
                 UHIDEventType.START: self.start_ev,
                 UHIDEventType.STOP: self.stop_ev,
                 UHIDEventType.OPEN: self.open_ev,
                 UHIDEventType.CLOSE: self.close_ev,
                 UHIDEventType.OUTPUT: self.output_ev,
-                UHIDEventType.GET_REPORT: self.report_ev
-            }.get(ev_type, self.error_ev)(ev_type, ev)
+                UHIDEventType.GET_REPORT: self.get_report_ev,
+                UHIDEventType.SET_REPORT: self.set_report_ev
+            }.get(ev_type, self.error_ev)(ev_type, ev_bytes)
 
     def destroy_ev(self, fd):
-        evBytes = bytes( UHIDInput2Event(type=UHIDEventType.DESTROY).pack() )
-        fd.write(evBytes)
+        ev = UHIDInput2Event(event=UHIDEventType.DESTROY.value, ev_len=0)
+        os.write(fd, ev.pack())
 
     def run(self):
         fd = None
+        started = False
         try:
-            fd = os.open(self.device_path, os.O_RDWR | os.O_CLOEXEC)
-            flags = fcntl.fcntl(fd, fcntl.F_GETFL)
-            fcntl.fcntl(fd, fcntl.F_SETFL, flags | os.O_NONBLOCK)
-            interrupt = False
+            fd = os.open('/dev/uhid', os.O_RDWR)  #| os.O_CLOEXEC| os.O_NONBLOCK
+            #fcntl.fcntl(fd, fcntl.F_SETFL, os.O_NONBLOCK)
         except OSError as e:
-            print("OSError with udev fd")
+            logging.debug("OSError with uhid fd")
+            import traceback
+            print(traceback.format_exc())
             return
         if fd == None:
-            print("Error with udev fd")
+            logging.debug("Error with udev fd")
             return
         try:
             #Send create
-            create_2_req = bytes(UHIDCreate2Event().pack())
-            os.write(create_2_req)
-            while not interrupt:
-                #Poll for event
-                try:
+            create_2_req = UHIDCreate2Event().pack()
+            #logging.debug(create_2_req)
+            n = os.write(fd, bytearray(create_2_req))
+            if n != len(create_2_req):
+                raise RuntimeError("invalid write length")
+            while not self.interrupt:
+                try: #Poll for event
                     eventBytes = os.read(fd, EV_MAX_SIZE)
-                    if isinstance(eventBytes, list) and eventBytes.length >= UHID_EVENT_TYPE_SIZE:
+                    self.log_received_bytes(eventBytes)
+                    if isinstance(eventBytes, bytes) \
+                            and len(eventBytes) >= UHID_EVENT_TYPE_SIZE:
                         eventType = UHIDEventType.from_bytes(
                                         eventBytes[:UHID_EVENT_TYPE_SIZE])
                         self.process_event(eventType, eventBytes)
+                    else:
+                        logging.error(f"Invalid event read :: {eventBytes}")
                 except BlockingIOError: #No event
-                    print("No data available (non-blocking read)")
-                if interrupt == True:
+                    logging.debug("No data available (non-blocking read)")
+                if self.interrupt == True:
                     break
-                #Send back queued events
-                if not eventQueue.empty():
+                if not self.pending.empty(): #Send back queued events
                     try:
-                        inData = inQ.get(true, 0.0001) #10ns
-                        ev = UHIDInput2Event(size=len(inData), data=inData)
-                        os.write(ev.pack())
+                        inData = self.pending.get(True, 0.00001) #10ns
+                        ev = UHIDInput2Event(ev_len=65, data=inData)
+                        inBytes = ev.pack()
+                        self.log_received_bytes(inBytes)
+                        n = os.write(fd, bytearray(inBytes))
+                        if n != len(inBytes):
+                            raise RuntimeError(f"invalid write length {n} != {len(ev.pack())}")
+                        else:
+                            logging.debug("Event sent!")
                     except queue.Empty:
-                        print("Could not get output event, not sending anything")
-            time.sleep(0.001) #poll every ms
+                        logging.debug("Could not get output event, not sending anything.")
+                if not self.interrupt:
+                    time.sleep(0.01) #poll every 10 ms
         finally:
-            if fd and fd.writable() == True:
+            if started:
                 self.destroy_ev(fd)
             os.close(fd)
-
-if __name__ == "__main__":
-    print("#TODO")
-    udev = UserDevice()
-    udev.start()
-    while udev.is_alive():
-        time.sleep(1)
