4372ea7e701942004b8e64a250ad52c312280662
Lachlan Gleeson <lgleeson@au1.ibm.com>
2025-06-12 13:53:03 +1000

    Migrate build to githib

--- setup.py
+++ setup.py
@@ -1,9 +1,37 @@
 from setuptools import find_packages, setup
 import os
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+class CleanCommand(Command):
+    """Custom clean command to tidy up the project root."""
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        for root, dirs, files in os.walk("./", topdown=False):
+            for name in files:
+                if name.endswith((".pyc", ".tgz", ".whl")):
+                    print("remove {}".format(os.path.join(root, name)))
+                    os.remove(os.path.join(root, name))
+            for name in dirs:
+                if name.endswith((".egg-info", "build", "dist", "__pycache__", "html")):
+                    print("remove {}".format(os.path.join(root, name)))
+                    #os.rmdir(os.path.join(root, name))
+                    os.system('rm -vrf {}'.format(os.path.join(root, name)))
+
+
+BUILD_ID = os.environ.get('TRAVIS_BUILD_NUMBER', 0)
+if BUILD_ID == 0:
+    BUILD_ID = os.environ.get('GITHUB_RUN_NUMBER', 0)
+
 setup(
     name='soft_fido2',
-    version='0.4.%s' % os.environ.get('TRAVIS_BUILD_NUMBER', 0),
+    version='0.4.%s' % BUILD_ID,
     description='Software based FIDO2 Authenticator',
     author='Lachlan Gleeson',
     author_email='lgleeson@au1.ibm.com',
@@ -17,5 +45,16 @@ setup(
         'PyJwt>=0.6.1'
     ],
     url='https://github.com/lachlan-ibm/soft-fido2',
-    zip_safe=False
+    project_urls={
+        'Homepage': 'https://github.com/lachlan-ibm/soft-fido2',
+        'Documentation': 'https://github.com/lachlan-ibm/soft-fido2/README.md',
+        'Source': 'https://github.com/lachlan-ibm/soft-fido2',
+        'Tracker': 'https://github.com/lachlan-ibm/soft-fido2/issues'
+    },
+    zip_safe=False,
+        cmdclass={
+        'clean': CleanCommand,
+    },
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
