bc70314f956776dff13099bf8102964d0773e0e2
AndyMars8 <andes.ma@griffithuni.edu.au>
2025-07-31 23:06:10 +1000

    Refactoring

--- util/manage_creds.py
+++ util/manage_creds.py
@@ -1,13 +1,14 @@
 import os, sys
 from soft_fido2.key_pair import KeyUtils
 from cryptography.hazmat.primitives import hashes
+from getpass import getpass
 
 if "FIDO_HOME" not in os.environ:
     print("FIDO_HOME not set")
     sys.exit(1)
 
-pin = input("Pin [0000]: ")
-pin = "0000" if pin == None or len(pin) == 0 else pin
+pin = getpass(prompt="Pin [00000000]: ")
+pin = "00000000" if pin == None or len(pin) == 0 else pin
 passkey = input("Passkey filename [default]: ")
 passkey = "default" if passkey == None or len(passkey) == 0 else passkey
 passkey = os.path.join(os.environ.get("FIDO_HOME"), passkey + '.passkey')
@@ -23,13 +24,13 @@ resCreds = d.get('res_creds', [])
 print(f"Passkey {passkey} in {os.environ.get("FIDO_HOME")} "\
         "can be validated with the provided pin! :)")
 newCreds = []
-prompt = "Remove credential for {} Y:[N]"
+prompt = "Remove credential for user {} on {}? Y:[N]"
 for cred in resCreds:
-    print("Credential: {}".format(cred))
-    for k, v in cred.items():
-        if k != 'user': #rpId
-            rsp = input(prompt.format(k))
-            if rsp.upper() != 'Y':
-                newCreds += [cred]
+    rpID = next(iter(cred))
+    rp, userID = cred[rpID], cred['user']
+    print("\nCredential for user \"{}\" on \"{}\"\n{}".format(userID, rpID, rp))
+    rsp = input(prompt.format(userID, rpID))
+    if rsp.upper() != 'Y':
+        newCreds += [cred]
 d['res_creds'] = newCreds
 KeyUtils._save_passkey(d, pinHash, passkey)
