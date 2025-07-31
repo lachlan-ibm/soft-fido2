40a797654f687dae96f9e408c329932540ad2231
AndyMars8 <andes.ma@griffithuni.edu.au>
2025-07-31 23:05:32 +1000

    Updated default pin appearance

--- util/verify_passkey.sh
+++ util/verify_passkey.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #Read password/pin
-echo -n "Pin [0000]: "
+echo -n "Pin [00000000]: "
 read -s READ_PIN
-PIN="${READ_PIN:-"0000"}"
+PIN="${READ_PIN:-"00000000"}"
 #Get file name
 echo -n -e "\nPasskey filename [default]: "
 read READ_PASSKEY
