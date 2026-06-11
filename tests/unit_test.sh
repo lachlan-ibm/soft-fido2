d6298c6bb529f5f917c3f5634319dfd3e908f336
Lachlan Gleeson <lgleeson@au1.ibm.com>
2026-06-11 12:21:28 +1000

    Git Action Build Defects

--- tests/unit_test.sh
+++ tests/unit_test.sh
@@ -3,9 +3,9 @@
 set -e
 
 #compile
-python setup.py sdist bdist_wheel
+#python setup.py sdist bdist_wheel
 #run tests
-export PYTHONPATH="$PYTHONPATH:$(pwd)/build/lib"
+#export PYTHONPATH="$PYTHONPATH:$(pwd)/build/lib"
 cd tests
 pytest attestation_unit_test.py
 pytest assertion_unit_test.py
